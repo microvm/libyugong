87743f8db8de71703a8aba57467c9ad72889abc9
Kunshan Wang <wks1986@gmail.com>
2016-02-11 17:49:57 +1100

    WIP: Prepare to introduce libunwind from LLVM

--- yugong-llvm/yugong-llvm.cpp
+++ yugong-llvm/yugong-llvm.cpp
@@ -1,1 +1,14 @@
 #include "yugong-llvm.hpp"
+
+namespace yg {
+    ProxyMemoryManager::ProxyMemoryManager(std::unique_ptr<llvm::SectionMemoryManager> mm):
+        mm(std::move(mm)), StackMapSection(nullptr), StackMapSectionSize(0LL) {}
+
+    
+    uint8_t *ProxyMemoryManager::allocateCodeSection(uintptr_t Size, unsigned Alignment, unsigned SectionID, llvm::StringRef SectionName) {
+        printf("allocateCodeSection. Size=%" PRIuPTR ", Alignment=%u, SectionID=%u, SectionName=%s\n", Size, Alignment,
+                SectionID, SectionName.str().c_str());
+        return mm->allocateCodeSection(Size, Alignment, SectionID, SectionName);
+
+    }
+}
--- yugong-llvm/yugong-llvm.hpp
+++ yugong-llvm/yugong-llvm.hpp
@@ -1,6 +1,44 @@
 #ifndef __YUGONG_LLVM_HPP__
 #define __YUGONG_LLVM_HPP__
 
-#include <yugong.hpp>
+#include "yugong.hpp"
+
+#include <llvm/ExecutionEngine/ExecutionEngine.h>
+#include <llvm/ExecutionEngine/MCJIT.h>
+#include <llvm/ExecutionEngine/RTDyldMemoryManager.h>
+#include <llvm/ExecutionEngine/SectionMemoryManager.h>
+
+#include <string>
+
+namespace yg {
+
+    class YGStackMapHelper {
+
+    };
+
+    class ProxyMemoryManager: public llvm::RTDyldMemoryManager {
+        ProxyMemoryManager(const ProxyMemoryManager&) = delete;
+        void operator=(const ProxyMemoryManager&) = delete;
+
+        std::unique_ptr<llvm::SectionMemoryManager> mm;
+
+        uint8_t *StackMapSection;
+        uintptr_t StackMapSectionSize;
+
+        public:
+        ProxyMemoryManager(std::unique_ptr<llvm::SectionMemoryManager> mm);
+
+        uint8_t *allocateCodeSection(uintptr_t Size, unsigned Alignment,
+                unsigned SectionID, llvm::StringRef SectionName) override;
+
+        uint8_t *allocateDataSection(uintptr_t Size, unsigned Alignment,
+                unsigned SectionID, llvm::StringRef SectionName,
+                bool isReadOnly) override;
+
+        bool finalizeMemory(std::string *ErrMsg = nullptr) override;
+
+        virtual void invalidateInstructionCache();
+    };
+}
 
 #endif // __YUGONG_LLVM_HPP__
